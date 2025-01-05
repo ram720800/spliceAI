d98bd58ac43665fbf5adaafd9ab03ddf260737e6
ram720800 <code72hub@gmail.com>
2025-01-05 19:06:53 +0530

    added hero section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Header } from "./sections/header";
+import { useState } from "react";
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+    </main>
+  );
+}
+export default App;
