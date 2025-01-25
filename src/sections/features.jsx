c99af879c9cd20260585406761ab03f92bee9a09
ram720800 <code72hub@gmail.com>
2025-01-25 23:02:06 +0530

    made some mobile responsive changes

--- src/sections/features.jsx
+++ src/sections/features.jsx
@@ -46,42 +46,77 @@ export const Features = () => {
                       className="size-17/20 object-contain z-20 "
                     />
                   </div>
-                  <h3 className="relative z-2 mx-auto max-w-32 my-0 base-small text-center uppercase">{ title}</h3>
+                  <h3 className="relative z-2 mx-auto max-w-32 my-0 base-small text-center uppercase">
+                    {title}
+                  </h3>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="relative max-md:hidden">
-            <h1 className="h3 text-p4 text-left my-24 w-[500px]">The Ecosystem behind Splice AI</h1>
-            <img src="/images/ecosystem.png" alt="ecosystem" className="w-[849px] h-[498px] ml-20 max-xl:hidden" />
-            <div className="relative flex justify-around items-center max-xl:hidden">
-              <div className="border border-p5 p-5 -translate-y-56">01</div>
-              <div className="border border-p5 p-5 -translate-y-[26rem]">02</div>
-              <div className="border border-p5 p-5 -translate-y-36 -translate-x-32">03</div>
-              <div className="border border-p5 p-5 -translate-y-[22rem] -translate-x-36">04</div>
+          <div className="relative">
+            <h1 className="h3 text-p4 text-left my-24 w-[500px] max-sm:h5 max-sm:w-[300px] mb-0">
+              The Ecosystem behind Splice AI
+            </h1>
+            <div className="">
+              <img
+                src="/images/ecosystem.png"
+                alt="ecosystem"
+                className="object-center object-contain w-[950px] h-[560px] max-sm:-mt-36 mt-12 mx-auto"
+              />
+              <div className="relative flex justify-around items-center">
+                <div className="border border-p5 p-5 max-sm:p-1 -translate-y-56 max-sm:-translate-y-64">
+                  01
+                </div>
+                <div className="border border-p5 p-5 max-sm:p-1 -translate-y-[28rem] max-sm:-translate-y-[23rem]">
+                  02
+                </div>
+                <div className="border border-p5 p-5 max-sm:p-1 -translate-y-36 -translate-x-32 max-sm:-translate-x-8 max-sm:-translate-y-60">
+                  03
+                </div>
+                <div className="border border-p5 p-5 max-sm:p-1 -translate-y-[24rem] -translate-x-16 max-sm:-translate-x-3 max-sm:-translate-y-[21rem] max-lg:-translate-x-8">
+                  04
+                </div>
+              </div>
             </div>
           </div>
-          
-          <div className="feature-after relative max-md:hidden">
-            <div className="grid grid-cols-2 gap-10 mt-10 text-start border-2 border-p4 p-8 rounded-lg" >
-              <div className="h4">AI Models & Tools</div>
-              <div className="body-2 ">Splice AI offers advanced AI models and tools explicitly designed for video editing</div>
-              <div className="h4">Customize your video</div>
-              <div className="body-2">Replace the AI avatar, colors, fonts, layouts and more with splice.</div>
-              <div className="h4">Collaborate</div>
-              <div className="body-2">Share your video and let others comment and collaborate</div>
-              <div className="h4">Share and export</div>
-              <div className="body-2"> Download your video, get a share link, or embed it anywhere.</div>
+
+          <div className="feature-after relative max-sm:-mt-36">
+            <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-10 mt-10 text-start border-2 border-p4 p-8 rounded-lg">
+              <div className="h4 max-sm:h5">AI Models & Tools</div>
+              <div className="body-2 max-sm:body-3">
+                Splice AI offers advanced AI models and tools explicitly
+                designed for video editing
+              </div>
+              <div className="h4 max-sm:h5">Customize your video</div>
+              <div className="body-2 max-sm:body-3">
+                Replace the AI avatar, colors, fonts, layouts and more with
+                splice.
+              </div>
+              <div className="h4 max-sm:h5">Collaborate</div>
+              <div className="body-2 max-sm:body-3">
+                Share your video and let others comment and collaborate
+              </div>
+              <div className="h4 max-sm:h5">Share and export</div>
+              <div className="body-2 max-sm:body-3">
+                Download your video, get a share link, or embed it anywhere.
+              </div>
             </div>
-          
-            <div className="relative flex justify-around items-center max-lg:hidden">
-              <div className="p-5 -translate-y-[26rem] -translate-x-[10.3rem]">01</div>
-              <div className="p-5 -translate-y-[19rem]  -translate-x-[28rem]">02</div>
-              <div className="p-5 -translate-y-[12.5rem] -translate-x-[45.5rem]">03</div>
-              <div className="p-5 -translate-y-[6rem] -translate-x-[63rem]">04</div>
+
+            <div className="relative flex justify-around items-center max-md:hidden">
+              <div className="p-5 max-sm:p-1 -translate-y-[26rem] -translate-x-[10.3rem]">
+                01
+              </div>
+              <div className="p-5 max-sm:p-1 -translate-y-[19rem]  -translate-x-[28rem]">
+                02
+              </div>
+              <div className="p-5 max-sm:p-1 -translate-y-[12.5rem] -translate-x-[45.5rem]">
+                03
+              </div>
+              <div className="p-5 max-sm:p-1 -translate-y-[6rem] -translate-x-[63rem]">
+                04
+              </div>
             </div>
           </div>
-          
         </div>
       </Element>
     </section>
